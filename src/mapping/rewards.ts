e9743828cb55675f1c2f9fd24c59d5ea65617713
Gregory Luneau <gregory.luneau@gmail.com>
2023-12-21 15:38:17 -0500

    refactor

--- src/mapping/rewards.ts
+++ src/mapping/rewards.ts
@@ -1,71 +1,47 @@
-import { Store } from "@subsquid/typeorm-store";
 import { RewardEvent, RewardEventType } from "../model";
-import { Event, ProcessorContext } from "../processor";
+import { Event } from "../processor";
 import { Entities, getSs58Address, getContractAddress } from "../utils";
 import { events } from "../types";
+
+// Function to create and add a reward event
+function createAndAddRewardEvent(event: Event, decodedData: any, eventType: RewardEventType, entities: Entities) {
+  const rewardEvent = new RewardEvent({
+    id: event.id,
+    transaction: eventType,
+    contractAddress: decodedData.smartContract ? getContractAddress(decodedData.smartContract) : undefined,
+    tierId: decodedData.tierId,
+    userAddress: getSs58Address(decodedData.account || decodedData.beneficiary),
+    era: decodedData.era ? BigInt(decodedData.era) : undefined,
+    amount: decodedData.amount,
+    period: decodedData.period,
+    timestamp: BigInt(event.block.timestamp || 0),
+    blockNumber: BigInt(event.block.height),
+  });
+  entities.RewardsToInsert.push(rewardEvent);
+}
 
 export async function handleRewards(
   event: Event,
   entities: Entities
 ): Promise<void> {
-  console.log("events", JSON.stringify(event));
 
   switch (event.name) {
     case events.dappStaking.reward.name:
       if (events.dappStaking.reward.v1.is(event)) {
-        const { account, era, amount } =
-          events.dappStaking.reward.v1.decode(event);
-        const rewardEvent = new RewardEvent({
-          id: event.id,
-          transaction: RewardEventType.Reward,
-          contractAddress: undefined,
-          tierId: undefined,
-          userAddress: getSs58Address(account),
-          era: BigInt(era),
-          amount,
-          period: undefined,
-          timestamp: BigInt(event.block.timestamp || 0),
-          blockNumber: BigInt(event.block.height),
-        });
-        entities.RewardsToInsert.push(rewardEvent);
+        const decodedData = events.dappStaking.reward.v1.decode(event);
+        createAndAddRewardEvent(event, decodedData, RewardEventType.Reward, entities);
       }
       break;
     case events.dappStaking.dAppReward.name:
       if (events.dappStaking.dAppReward.v1.is(event)) {
-        const { beneficiary, smartContract, tierId, era, amount } =
-          events.dappStaking.dAppReward.v1.decode(event);
-        const rewardEvent = new RewardEvent({
-          id: event.id,
-          transaction: RewardEventType.DAppReward,
-          contractAddress: getContractAddress(smartContract),
-          tierId,
-          userAddress: getSs58Address(beneficiary),
-          era: BigInt(era),
-          amount,
-          period: undefined,
-          timestamp: BigInt(event.block.timestamp || 0),
-          blockNumber: BigInt(event.block.height),
-        });
-        entities.RewardsToInsert.push(rewardEvent);
+        const decodedData = events.dappStaking.dAppReward.v1.decode(event);
+        createAndAddRewardEvent(event, decodedData, RewardEventType.DAppReward, entities);
       }
       break;
     case events.dappStaking.bonusReward.name:
       if (events.dappStaking.bonusReward.v1.is(event)) {
-        const { account, smartContract, period, amount } =
-          events.dappStaking.bonusReward.v1.decode(event);
-        const rewardEvent = new RewardEvent({
-          id: event.id,
-          transaction: RewardEventType.BonusReward,
-          contractAddress: getContractAddress(smartContract),
-          tierId: undefined,
-          userAddress: getSs58Address(account),
-          era: undefined,
-          amount,
-          period,
-          timestamp: BigInt(event.block.timestamp || 0),
-          blockNumber: BigInt(event.block.height),
-        });
-        entities.RewardsToInsert.push(rewardEvent);
+        const decodedData = events.dappStaking.bonusReward.v1.decode(event);
+        createAndAddRewardEvent(event, decodedData, RewardEventType.BonusReward, entities);
       }
       break;
     default:
