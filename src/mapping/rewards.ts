140b07a0c2df2eccebf3be14f9d945e9f629f226
Gregory Luneau <gregory.luneau@gmail.com>
2023-12-21 08:34:04 -0500

    bonus and dapp rewards

--- src/mapping/rewards.ts
+++ src/mapping/rewards.ts
@@ -1,7 +1,7 @@
 import { Store } from "@subsquid/typeorm-store";
 import { RewardEvent, RewardEventType } from "../model";
 import { Event, ProcessorContext } from "../processor";
-import { Entities, getSs58Address } from "../utils";
+import { Entities, getSs58Address, getContractAddress } from "../utils";
 import { events } from "../types";
 
 export async function handleRewards(
@@ -10,13 +10,12 @@ export async function handleRewards(
 ): Promise<void> {
   console.log("events", JSON.stringify(event));
 
-  let rewardEvent: RewardEvent;
   switch (event.name) {
     case events.dappStaking.reward.name:
       if (events.dappStaking.reward.v1.is(event)) {
         const { account, era, amount } =
           events.dappStaking.reward.v1.decode(event);
-        rewardEvent = {
+        const rewardEvent = new RewardEvent({
           id: event.id,
           transaction: RewardEventType.Reward,
           contractAddress: undefined,
@@ -27,7 +26,45 @@ export async function handleRewards(
           period: undefined,
           timestamp: BigInt(event.block.timestamp || 0),
           blockNumber: BigInt(event.block.height),
-        };
+        });
+        entities.RewardsToInsert.push(rewardEvent);
+      }
+      break;
+    case events.dappStaking.dAppReward.name:
+      if (events.dappStaking.dAppReward.v1.is(event)) {
+        const { beneficiary, smartContract, tierId, era, amount } =
+          events.dappStaking.dAppReward.v1.decode(event);
+        const rewardEvent = new RewardEvent({
+          id: event.id,
+          transaction: RewardEventType.DAppReward,
+          contractAddress: getContractAddress(smartContract),
+          tierId,
+          userAddress: getSs58Address(beneficiary),
+          era: BigInt(era),
+          amount,
+          period: undefined,
+          timestamp: BigInt(event.block.timestamp || 0),
+          blockNumber: BigInt(event.block.height),
+        });
+        entities.RewardsToInsert.push(rewardEvent);
+      }
+      break;
+    case events.dappStaking.bonusReward.name:
+      if (events.dappStaking.bonusReward.v1.is(event)) {
+        const { account, smartContract, period, amount } =
+          events.dappStaking.bonusReward.v1.decode(event);
+        const rewardEvent = new RewardEvent({
+          id: event.id,
+          transaction: RewardEventType.BonusReward,
+          contractAddress: getContractAddress(smartContract),
+          tierId: undefined,
+          userAddress: getSs58Address(account),
+          era: undefined,
+          amount,
+          period,
+          timestamp: BigInt(event.block.timestamp || 0),
+          blockNumber: BigInt(event.block.height),
+        });
         entities.RewardsToInsert.push(rewardEvent);
       }
       break;
