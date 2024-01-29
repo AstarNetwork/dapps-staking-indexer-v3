540cb7e28e761b176b89919643bd8c3214805619
Gregory Luneau <gregory.luneau@gmail.com>
2024-01-29 18:32:48 -0500

    shiden conf

--- src/processor.ts
+++ src/processor.ts
@@ -1,63 +1,73 @@
 import {
-    BlockHeader,
-    SubstrateBatchProcessor,
-    SubstrateBatchProcessorFields,
-    DataHandlerContext,
-    Event as _Event,
-    Call as _Call,
-    Extrinsic as _Extrinsic
-} from '@subsquid/substrate-processor'
-import {assertNotNull} from '@subsquid/util-internal'
-import {lookupArchive} from '@subsquid/archive-registry'
+  BlockHeader,
+  SubstrateBatchProcessor,
+  SubstrateBatchProcessorFields,
+  DataHandlerContext,
+  Event as _Event,
+  Call as _Call,
+  Extrinsic as _Extrinsic,
+} from "@subsquid/substrate-processor";
+import { assertNotNull } from "@subsquid/util-internal";
+import { lookupArchive } from "@subsquid/archive-registry";
+import { events } from "./types";
 
-import {events} from './types'
+let CHAIN = "shiden";
+
+let chainURL;
+let blockRange;
+switch (CHAIN) {
+  case "astar":
+    blockRange = { from: 4_342_300 };
+    chainURL = "wss://astar-rpc.dwellir.com";
+    break;
+  case "shiden":
+    blockRange = { from: 5_820_969 };
+    chainURL = "wss://shiden-rpc.dwellir.com";
+    break;
+  case "shibuya":
+    blockRange = { from: 5_335_615 };
+    chainURL = "wss://shibuya-rpc.dwellir.com";
+    break;
+}
 
 export const processor = new SubstrateBatchProcessor()
-    .setDataSource({
-        // chain: {
-        //     url: assertNotNull(process.env.RPC_ENDPOINT),
-        //     rateLimit: 10
-        // }
-        // chain: 'wss://rpc.shibuya.astar.network',
-        chain: 'wss://shibuya-rpc.dwellir.com',
-        // chain: 'wss://shibuya.api.onfinality.io/public-ws',
-        // chain: 'wss://shibuya.public.blastapi.io/',
-        // archive: lookupArchive('shibuya', {type: 'Substrate', release: 'ArrowSquid'})
-    })
-    .addEvent({
-        name: [
-            events.dappsStaking.withdrawn.name,
-            events.dappsStaking.withdrawFromUnregistered.name,
-            events.dappsStaking.bondAndStake.name,
-            events.dappsStaking.nominationTransfer.name,
-            events.dappsStaking.unbondAndUnstake.name,
-            events.dappStaking.dAppRegistered.name,
-            events.dappStaking.dAppUnregistered.name,
-            events.dappStaking.dAppOwnerChanged.name,
-            events.dappStaking.dAppRewardDestinationUpdated.name,
-            events.dappStaking.locked.name,
-            events.dappStaking.unlocking.name,
-            // events.dappStaking.claimedUnlocked.name,
-            events.dappStaking.relock.name,
-            events.dappStaking.stake.name,
-            events.dappStaking.unstake.name,
-            events.dappStaking.unstakeFromUnregistered.name,
-            events.dappStaking.newSubperiod.name,
-            events.dappStaking.reward.name,
-            events.dappStaking.bonusReward.name,
-            events.dappStaking.dAppReward.name,
-        ]
-    })
-    .setFields({
-        block: {
-            timestamp: true
-        }
-    })
-    .setBlockRange({from: 5_335_615})
+  .setDataSource({
+    chain: chainURL!,
+  })
+  .addEvent({
+    name: [
+      events.dappsStaking.withdrawn.name,
+      events.dappsStaking.withdrawFromUnregistered.name,
+      events.dappsStaking.bondAndStake.name,
+      events.dappsStaking.nominationTransfer.name,
+      events.dappsStaking.unbondAndUnstake.name,
+      events.dappStaking.dAppRegistered.name,
+      events.dappStaking.dAppUnregistered.name,
+      events.dappStaking.dAppOwnerChanged.name,
+      events.dappStaking.dAppRewardDestinationUpdated.name,
+      events.dappStaking.locked.name,
+      events.dappStaking.unlocking.name,
+      // events.dappStaking.claimedUnlocked.name,
+      events.dappStaking.relock.name,
+      events.dappStaking.stake.name,
+      events.dappStaking.unstake.name,
+      events.dappStaking.unstakeFromUnregistered.name,
+      events.dappStaking.newSubperiod.name,
+      events.dappStaking.reward.name,
+      events.dappStaking.bonusReward.name,
+      events.dappStaking.dAppReward.name,
+    ],
+  })
+  .setFields({
+    block: {
+      timestamp: true,
+    },
+  })
+  .setBlockRange(blockRange);
 
-export type Fields = SubstrateBatchProcessorFields<typeof processor>
-export type Block = BlockHeader<Fields>
-export type Event = _Event<Fields>
-export type Call = _Call<Fields>
-export type Extrinsic = _Extrinsic<Fields>
-export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>
+export type Fields = SubstrateBatchProcessorFields<typeof processor>;
+export type Block = BlockHeader<Fields>;
+export type Event = _Event<Fields>;
+export type Call = _Call<Fields>;
+export type Extrinsic = _Extrinsic<Fields>;
+export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>;
