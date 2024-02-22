a76da13547172163a0fb08fae65bda11bdb91174
Gregory Luneau <gregory.luneau@gmail.com>
2024-02-22 09:31:30 -0500

    dedicated

--- src/processor.ts
+++ src/processor.ts
@@ -12,21 +12,22 @@ import { lookupArchive } from "@subsquid/archive-registry";
 import { events } from "./types";
 
 const blockRange = { from: parseInt(process.env.BLOCK_RANGE!, 10) };
-const archive =
-  process.env.ARCHIVE != ""
-    ? lookupArchive(process.env.ARCHIVE!, {
-        type: "Substrate",
-        release: "ArrowSquid",
-      })
-    : undefined;
-console.log(`Archive: ${archive}`);
 console.log(`Block Range: ${blockRange.from}`);
-console.log(`Chain URL: ${process.env.RPC_ENDPOINT}`);
+// const archive =
+//   process.env.ARCHIVE != ""
+//     ? lookupArchive(process.env.ARCHIVE!, {
+//         type: "Substrate",
+//         release: "ArrowSquid",
+//       })
+//     : undefined;
+// console.log(`Archive: ${archive}`);
+const chain = process.env.RPC_ASTAR_SUBSTRATE_HTTP || process.env.RPC_ENDPOINT;
+console.log(`Chain URL: ${chain}`);
 
 export const processor = new SubstrateBatchProcessor()
   .setDataSource({
-    chain: process.env.RPC_ENDPOINT!,
-    archive: archive,
+    chain: assertNotNull(chain),
+    // archive: archive,
   })
   .addEvent({
     name: [
