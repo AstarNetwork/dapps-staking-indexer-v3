0813ec1daebf866c3786ffe3616be7c0cbb35f94
Bobo <bobo.kovacevic@gmail.com>
2024-11-08 15:12:05 +0100

    Cleanup

--- src/mapping/protocolState.ts
+++ src/mapping/protocolState.ts
@@ -8,34 +8,6 @@ export const IS_SHIBUYA = process.env.ARCHIVE === "shibuya";
 let currentPeriod: number | undefined;
 const eraPeriodMappingCache = new Map<number, number>();
 
-function checkEnv() {
-  if (!process.env.V3_FIRST_BLOCK) {
-    throw new Error("V3_FIRST_BLOCK is not set");
-  }
-
-  if (!process.env.V3_PERIOD_LENGTH) {
-    throw new Error("V3_PERIOD_LENGTH is not set");
-  }
-
-  if (!process.env.V3_FIRST_ERA) {
-    throw new Error("V3_FIRST_ERA is not set");
-  }
-
-  if (!process.env.V3_ERAS_PER_PERIOD) {
-    throw new Error("V3_ERAS_PER_PERIOD is not set");
-  }
-}
-
-// export function getPeriodForBlock(blockNumber: number): number {
-//   checkEnv();
-//   return (
-//     Math.floor(
-//       (blockNumber - Number(process.env.V3_FIRST_BLOCK)) /
-//         Number(process.env.V3_PERIOD_LENGTH)
-//     ) + 1
-//   );
-// }
-
 async function getLastPeriodMapping(
   ctx: ProcessorContext<Store>
 ): Promise<EraPeriodMapping | undefined> {
@@ -72,17 +44,6 @@ export async function getCurrentPeriod(
 
 export function setCurrentPeriod(period: number): void {
   currentPeriod = period;
-}
-
-function getPeriodForEra(eraNumber: number): number {
-  checkEnv();
-  const result =
-    Math.floor(
-      (eraNumber - Number(process.env.V3_FIRST_ERA)) /
-        Number(process.env.V3_ERAS_PER_PERIOD)
-    ) + 1;
-
-  return result;
 }
 
 export async function getPeriodForEraFromDatabase(
